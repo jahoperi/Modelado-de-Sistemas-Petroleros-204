950cc5b3e164ec5b825f0da0eb3555d37d0b2cda
jahoperi <97061564+jahoperi@users.noreply.github.com>
2022-04-28 21:50:43 -0500

    Update app_204_a.py

--- app_204_a.py
+++ app_204_a.py
@@ -12,7 +12,7 @@ import streamlit.components.v1 as components
 import requests  # pip install requests
 import plotly.express as px
 
-#import pandas as pd
+import pandas as pd
 #import numpy as np
 
 #import plotly.graph_objects as go 
@@ -31,9 +31,15 @@ page_names = ['Ángel Moras Conde', 'Antonio Pérez Rocha', 'Armando Nava Cedill
 page = st.radio('Navegación', page_names, index = 0)
 #st.write("**La variable 'page' returns:**", page)
 
-#df = pd.read_excel("NDR_PC-2.xlsx")
-#st.title("Datos")
-#st.write(df)
+data = pd.read_csv("NDR.csv")
+data = data.set_index('PROFESIONISTAS')
+
+#col1, col2, col3 = st.columns(3)
+
+col1, col2 = st.columns([100, 100])
+
+col1.subheader("DATOS")
+col1.write(data)
 
 #1                    
 if page == 'Ángel Moras Conde':
